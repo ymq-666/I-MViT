39bb773de63a4d9eb25f0781f58febe0fc21d442
YMQ666 <yangmianqing123@163.com>
2024-10-09 17:24:13 +0800

    train2.py

--- train2.py
+++ train2.py
@@ -127,14 +127,8 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=)
     parser.add_argument('--lr', type=float, default=)
     parser.add_argument('--num_workers', type=int, default=)
-
-    # 数据集所在根目录
     parser.add_argument('--data_path', type=str, default=r" ")
-
-    # 预训练权重路径，如果不想载入就设置为空字符
     parser.add_argument('--weights', type=str, default='')
-
-    # 设备
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
 
     opt = parser.parse_args()
